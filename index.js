f56c9e62feef9305d55e2ab6afbfd4151201d87d
Jesse Ilc <jesseilc123@gmail.com>
2023-02-02 12:33:29 -0600

    Lab complete

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer = "rob"
+
+function upperCaseCustomerName(){
+    customerName = "BOB";
+
+    return customerName;
+};
+
+
+function setBestCustomer(){
+    bestCustomer = "not bob";
+
+    return bestCustomer
+    
+};
+
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob";
+
+    return bestCustomer;
+};
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "toby"
+
+    return leastFavoriteCustomer
+}
